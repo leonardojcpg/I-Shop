70bcf81f6eea3f75b3cb6b3baf8b7cf240179b7e
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-03-03 00:54:35 -0300

    starting stitches style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
 import { globalStyles } from '@/styles/global'
+import { Container, Header } from '@/styles/pages/app'
 import type { AppProps } from 'next/app'
+import logoImg from '../assets/logo.svg'
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  globalStyles();
-  return <Component {...pageProps} />
+  
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
